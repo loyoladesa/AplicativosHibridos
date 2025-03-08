46182327045a181161c941f4a457b13299626e47
loyol <loyoladesa@gmail.com>
2025-03-07 21:37:46 -0300

    useState

--- App.tsx
+++ App.tsx
@@ -1,13 +1,67 @@
-import { Text, Image, TextInput, View} from "react-native";
+import React, {useState} from "react";
+import { Text, Image, TextInput, View, Button, ToastAndroid} from "react-native";
 
 
+
+
+
+function getMyName ():string {
+    const primeiro = "Sidney ";
+    const segundo  = "Loyola"    
+
+    console.log("Passei Aqui!");
+
+    return primeiro + segundo;
+    
+}
+
+const Logo = () => {
+  return (
+    <View>
+      <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} 
+            style={{width:200,height:200}}>
+      </Image>      
+    </View>
+  );
+}
+
+const Nome = () => {
+  
+
+  const nomeCompleto = "Nome";
+
+  return (
+    <View>
+      <Text style={{fontSize:20,marginBottom:15, marginTop:15}}>{nomeCompleto}</Text>
+    </View>
+  );
+}
 
 
 const App = () => {
+
+  const name = "Sidney Loyola";
+  const [trocar,setTrocar] = useState("nova");
+
+  const click = () => {
+
+    if (trocar == "nova"){
+      setTrocar("outra")
+    }else{
+      setTrocar("nova")
+    }
+    
+    
+    console.log(trocar)
+  }
+  
+
   return (
     
       <View>
-            <Text> Qual o seu nome?  </Text>
+            <Text style={{fontSize:25}}> Primeiro App  </Text>
+            <Logo></Logo>          
+
             <TextInput style={{
               color:"white",
               backgroundColor:"blue",
@@ -15,11 +69,14 @@ const App = () => {
               marginRight:8}}>
                 Cursor: 
             </TextInput>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} 
-            style={{width:300,height:300}}
-            ></Image>
-      </View>
-    
+            <Text style={{fontSize:20}}>{trocar}</Text>
+            
+            <Button
+              title="Trocar Nome"
+              onPress={click}>
+
+            </Button>
+      </View>   
 
   );
 }
